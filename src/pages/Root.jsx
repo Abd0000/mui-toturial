e5fa46ec6f2d633931a199ef5c35ad278b9a2365
abd000 <abdofared1000@gmail.com>
2024-02-10 02:00:41 +0200

    learning typography properties (lesson3)

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Typography from '@mui/material/Typography'
+
 
 const Root = () => {
   return (
     <div>
-      <h1>Hiiiiiiii</h1>
+     <Typography gutterBottom={true} variant="h4" component={"p"} >Header</Typography>
+      <Typography  noWrap variant="h4" component={"p"} >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam vitae fugit voluptate nulla sunt. Nam in rem quisquam debitis inventore asperiores ad culpa tempora, modi veritatis officia, vel saepe possimus?</Typography>
       <Outlet />
     </div>
   )
