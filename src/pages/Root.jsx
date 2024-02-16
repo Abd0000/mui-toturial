7fcc07edd90780edd27d04080c1e59ef1a6ae1fb
abd000 <abdofared1000@gmail.com>
2024-02-16 16:42:23 +0200

    styling component using 4 ways 1- plain css 2- sx property 3- using properties 4- using theme colors

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Typography from '@mui/material/Typography'
-
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Typography from "@mui/material/Typography";
 
 const Root = () => {
   return (
     <div>
-     <Typography gutterBottom={true} variant="h4" component={"p"} >Header</Typography>
-      <Typography  noWrap variant="h4" component={"p"} >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam vitae fugit voluptate nulla sunt. Nam in rem quisquam debitis inventore asperiores ad culpa tempora, modi veritatis officia, vel saepe possimus?</Typography>
+      <Typography
+        className="header"
+        gutterBottom={true}
+        variant="h4"
+        component={"p"}
+      >
+        Header
+      </Typography>
+      {/* //using sx prop to style the typography component */}
+      {/* using properties to style the typography like padding , width ,bgcolor and display */}
+      <Typography
+        sx={{ p: "10px", width: "100%" }}
+        p={"20px"}
+        bgcolor={"grey.200"}
+        display={"flex"}
+        justifyContent={"center"}
+        noWrap={false}
+        variant="h6"
+        component={"p"}
+        color={"secondary.main"} //using theme color to style the typography and it will change according to the theme( light or dark)
+      >
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam vitae
+        fugit voluptate nulla sunt. Nam in rem quisquam debitis inventore
+        asperiores ad culpa tempora, modi veritatis officia, vel saepe possimus?
+      </Typography>
       <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Root
+export default Root;
