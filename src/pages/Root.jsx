192a1ec5d774ec708005bf8b5936a5ebd53d3742
abd000 <abdofared1000@gmail.com>
2024-02-16 17:34:22 +0200

    learning how to use Button and IconButton elements and how to style them

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { ClearOutlined, MenuOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 
 const Root = () => {
   return (
@@ -11,12 +14,16 @@ const Root = () => {
         variant="h4"
         component={"p"}
       >
+        {/* using the IconButton component to create a button with icon */}
+        <IconButton size="large" sx={{position:"absolute" , top:"10px", left: "10px"}}>
+          <MenuOutlined color="info" fontSize="large" />
+        </IconButton>
         Header
       </Typography>
       {/* //using sx prop to style the typography component */}
       {/* using properties to style the typography like padding , width ,bgcolor and display */}
-      <Typography
-        sx={{ p: "10px", width: "100%" }}
+      <Typography 
+        sx={{ p: "10px", width: "100%", ml: "20px auto", mr: "20px auto" }}
         p={"20px"}
         bgcolor={"grey.200"}
         display={"flex"}
@@ -30,6 +37,16 @@ const Root = () => {
         fugit voluptate nulla sunt. Nam in rem quisquam debitis inventore
         asperiores ad culpa tempora, modi veritatis officia, vel saepe possimus?
       </Typography>
+      {/* using button element and using the endIcont and StartIcon */}
+      <Button
+        variant="contained"
+        color="error"
+        sx={{ ml: "auto", mr: "auto", display: "flex", mt: "10px" }}
+        startIcon={<ClearOutlined />}
+      >
+        Delete
+      </Button>
+
       <Outlet />
     </div>
   );
