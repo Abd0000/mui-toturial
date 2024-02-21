7926366af6e2f4c33d633d70216283c60185f288
abd000 <abdofared1000@gmail.com>
2024-02-22 01:32:13 +0200

    container part

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "components/Navbar";
 import Sidebar from "components/Sidebar";
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
 const drawerWidth = 240;
 const Root = () => {
   return (
     <div>
       <Navbar drawerWidth={drawerWidth} />
       <Sidebar drawerWidth={drawerWidth} />
-      <Box className="border" sx={{ml:`${drawerWidth}px`}}>
+      <Container sx={{ml:`${drawerWidth}px`}}>
         <Outlet />
-      </Box>
+      </Container>
     </div>
   );
 };
